f4482402da84990f60f507c9923ff3c586b088b1
Noah <justforgmini@gmail.com>
2026-02-24 09:48:59 +1100

    Create Interfaces.cs

---
+++ Windows/Core/Interfaces.cs
@@ -0,0 +1,54 @@
+namespace StartupDashboard.Core
+{
+    public interface IAuthService
+    {
+        bool Authenticate(string password);
+        void SetPassword(string newPassword);
+        bool IsPasswordSet();
+    }
+
+    public interface IMediaService
+    {
+        MediaInfo GetCurrentMedia();
+        event EventHandler<MediaInfo> MediaChanged;
+    }
+
+    public class MediaInfo
+    {
+        public string Title { get; set; } = "";
+        public string Artist { get; set; } = "";
+        public byte[]? AlbumArt { get; set; }
+        public bool IsPlaying { get; set; }
+    }
+
+    public interface IAppService
+    {
+        IEnumerable<AppEntry> GetInstalledApps();
+        void LaunchApp(AppEntry? app);
+    }
+
+    public class AppEntry
+    {
+        public string Name { get; set; } = "";
+        public string ExecutablePath { get; set; } = "";
+        public byte[]? Icon { get; set; }
+    }
+
+    public interface IStartupService
+    {
+        bool IsRegisteredForStartup();
+        void RegisterForStartup();
+        void UnregisterFromStartup();
+    }
+
+    public class SystemStats
+    {
+        public double CpuUsage { get; set; }
+        public double MemoryUsage { get; set; }
+    }
+
+    public interface ISystemMonitorService
+    {
+        SystemStats GetStats();
+    }
+}
